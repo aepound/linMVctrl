7e163c6a4cb2d5bcb38118cff2bcc18a689a0435
Andrew Pound <aepound@gmail.com>
2014-12-03 09:52:47 -0700

    Where I got the other night.
    I'll be starting from this point today, hopefully getting fairly
    far...

--- proj/matlab/moCap.m
+++ proj/matlab/moCap.m
@@ -1,10 +1,16 @@
 %Playing with the data...
 close all 
 clear all
+
+%% ACCAD OSU data ( *.txt format)
+% This was the first motion captuer data that I found.
+
 motCap = importdata('../data/accad/Aug210106.txt');
 
 cols = cellfun(@(x) strrep(x,'EricAutoLabel:',''),motCap.colheaders,'uni',false);
 cols = cols(3:end);
+% Funny: Apparently the name of the guy running is Eric... :)
+
 % Extract the time
 t = motCap.data(:,2);
 
@@ -28,6 +34,9 @@ end
 dl = d(:,3:20,:);
 dr = d(:,21:39,:);
 
+% The indices of the different markers placed on the runner:
+% (Painstakingly found by Andrew by trial and error plotting and 
+%  deciphering of the cryptic labels!)
 forehead = [7 26];  
 backhead = [4 23];
 ankles = [3 21];
@@ -52,7 +61,7 @@ clavicle = [2];
 neck = [1];
 
 
-
+% Plot the motion of the back over time.
 figure(3)
 clf(3)
 for iter= 1:size(d,2)
@@ -74,7 +83,8 @@ end
 
 
 
-%% Let's look now at a few of the time series.
+%% Take a look at the back as it is moving through time.
+% Let's look now at a few of the time series.
 
 inds1 = [backWaist fliplr(shoulders)];
 arminds = [shoulders; elbows; wristA];
@@ -90,12 +100,12 @@ clf(5)
 for iter = 50:size(d,1)
     if ~any(isnan(d(iter,inds1,:)))
         if exist('fh','var')
-            delete(fh)
+            %delete(fh)
         end
         ys = d(iter,inds1,2);
-        mny = mean(ys);
+        mny = 0; %mean(ys);
         xs = d(iter,inds1,1);
-        mnx = mean(xs);
+        mnx = 0; %mean(xs);
         zs = d(iter,inds1,3);
         mnz = mean(zs);
         
@@ -147,8 +157,8 @@ for iter = 50:size(d,1)
         
         %if iter == 1
         zlabel('z');zlim([-900 300])
-        ylabel('y');ylim([-400 400])
-        xlabel('x');xlim([-400 400])
+        ylabel('y');%ylim([-400 400])
+        xlabel('x');%xlim([-400 400])
         %end
         pause(.1)
     end
@@ -160,12 +170,22 @@ if ~plotarmslegs
     delete(fh)
 end
 
-%%  Should we see about some of the other motion capture files?
+%%  MoCapClub data ( *.c3d format)
+% Should we see about some of the other motion capture files?
 %
 
 datadir = '../data/mocapclub/Game_Package_c3d';
+fname = [datadir '/Jog_1.c3d'];
+%fname = [datadir '/Run_1.c3d'];
+%fname = [datadir '/Run_2.c3d'];
 
-fname = [datadir '/Run_1.c3d'];
+%datadir = '../data/mocapclub/Miscellaneous_Motions_c3d';
+%fname = [datadir '/ActorB_Run.c3d'];
+%fname = [datadir '/ActorB_Run_2.c3d'];
+%fname = [datadir '/ActorG_Run.c3d'];
+
+%datadir = '../data/mocapclub/Circus_Package_c3d';
+%fname = [datadir '/Human_Run_DW.c3d'];
 
 [Markers,VideoFrameRate,AnalogSignals,AnalogFrameRate, ...
     Event,ParameterGroup,CameraInfo,ResidualError]=readc3d(fname);
@@ -176,12 +196,48 @@ rshoulder = strmatch('RShoulder',markerlbls);
 lshoulder = strmatch('LShoulder',markerlbls);
 rhip      = strmatch('RightHip',markerlbls,'exact');
 lhip      = strmatch('LeftHip',markerlbls,'exact');
+if isempty(rhip)
+    rhip      = strmatch('RBackWaist',markerlbls,'exact');
+    lhip      = strmatch('LBackWaist',markerlbls,'exact');
+end
 
+
+lelbow    = strmatch('LeftElbow',markerlbls,'exact');
+relbow    = strmatch('RightElbow',markerlbls,'exact');
+lwrist    = strmatch('LeftWrist',markerlbls,'exact');
+rwrist    = strmatch('RightWrist',markerlbls,'exact');
+
+if isempty(lelbow)
+    lelbow    = strmatch('LOuterElbow',markerlbls,'exact');
+    relbow    = strmatch('ROuterElbow',markerlbls,'exact');
+    lwrist    = strmatch('LHand',markerlbls,'exact');
+    rwrist    = strmatch('RHand',markerlbls,'exact');
+end
+
+lknee     = strmatch('LeftKnee',markerlbls,'exact');
+rknee     = strmatch('RightKnee',markerlbls,'exact');
+lankle    = strmatch('LeftAnkle',markerlbls,'exact');
+rankle    = strmatch('RightAnkle',markerlbls,'exact');
+if isempty(lknee)
+    lknee     = strmatch('LOuterKnee',markerlbls,'exact');
+    rknee     = strmatch('ROuterKnee',markerlbls,'exact');
+    lankle    = strmatch('LAnkle',markerlbls,'exact');
+    rankle    = strmatch('RAnkle',markerlbls,'exact');
+end    
+ltoes     = strmatch('LToe',markerlbls,'exact');
+rtoes     = strmatch('RToe',markerlbls,'exact');
+
+
+arminds2 = [lshoulder rshoulder; lelbow relbow; lwrist rwrist];
+leginds2 = [lhip rhip; lknee rknee; lankle rankle; ltoes rtoes];
+
+disp('size of data:')
+disp(size(Markers))
 %%
 clear fh
 
 inds = [lshoulder rshoulder rhip lhip];
-
+cmap = jet(size(Markers,1));
 figure(1)
 clf(1)
 
@@ -189,25 +245,72 @@ mxnx = [-300 300];
 mxny = [-200 200];
 mxnz = [-300 300];
 for iter = 1:size(Markers,1)
-    if ~any(isnan(Markers(iter,inds,:)))
+    if ~all(any(isnan(Markers(iter,inds,:))))
         if exist('fh','var')
-            delete(fh)
+            %delete(fh)
         end
         ys = Markers(iter,inds,2);
-        mny = mean(ys);
+        mny = 0; %mean(ys);
         xs = Markers(iter,inds,1);
-        mnx = mean(xs);
+        mnx = 0; %mean(xs);
         zs = Markers(iter,inds,3);
         mnz = mean(zs);
-        fh = fill3(xs - mnx,ys - mny,zs-mnz,'r');
+        
+        
+        
+        
+        x0 = xs - mnx;
+        y0 = ys - mny;
+        z0 = zs - mnz;
+        fh = fill3(x0,y0,z0,'r');
+        hold on, grid on
+        G = [x0; y0; z0]';
+        [u, s, v] = svd(G,0);
+        
+        norm_len = 300;
+        normal = v(:,end); unormal = normal./norm(normal);
+        normal = unormal*.5*norm_len*-sign(unormal(1));
+        
+        ph = plot3([0 normal(1)]',[0 normal(2)]',[0 normal(3)]',...
+                'Color',cmap(iter,:),'linewidth',2);       
+        
+            
+        if plotarmslegs
+            if exist('armhandle2','var') && all(ishandle(armhandle2))
+                delete(armhandle2)
+            end
+            if exist('leghandle2','var') && all(ishandle(leghandle2))
+                delete(leghandle2)
+            end
+                
+            larm = bsxfun(@minus,squeeze(Markers(iter,arminds2(:,1),:)),[mnx mny mnz]);
+            rarm = bsxfun(@minus,squeeze(Markers(iter,arminds2(:,2),:)),[mnx mny mnz]);
+            
+            lleg = bsxfun(@minus,squeeze(Markers(iter,leginds2(:,1),:)),[mnx mny mnz]);
+            rleg = bsxfun(@minus,squeeze(Markers(iter,leginds2(:,2),:)),[mnx mny mnz]);
+            
+            armhandle2 = plot3([larm(:,1) rarm(:,1)]...
+                             ,[larm(:,2) rarm(:,2)]...
+                             ,[larm(:,3) rarm(:,3)]...
+                             ,'Color','g'...
+                             ,'linewidth',2);
+                         
+            leghandle2 = plot3([lleg(:,1) rleg(:,1)]...
+                             ,[lleg(:,2) rleg(:,2)]...
+                             ,[lleg(:,3) rleg(:,3)]...
+                             ,'Color','g'...
+                             ,'linewidth',2);
+                  
+        end
+        
         %if iter == 1
-        %zlabel('z');zlim([-300 300])
-        %ylabel('y');ylim([-400 400])
-        %xlabel('x');xlim([-400 400])
+        zlabel('z');zlim([-1700 500])
+        ylabel('y');%ylim([-400 400])
+        xlabel('x');%xlim([-400 400])
         %end
-        zlim(mxnz);
-        ylim(mxny);
-        xlim(mxnx);
+        %zlim(mxnz);
+        %ylim(mxny);
+        %xlim(mxnx);
 %        mxnx = [min(xl(1),mxnx(1)), max(xl(2),mxnx(2))]; 
 %        mxny = [min(yl(1),mxny(1)), max(yl(2),mxny(2))]; 
 %        mxnz = [min(zl(1),mxnz(1)), max(zl(2),mxnz(2))]; 
@@ -216,6 +319,20 @@ for iter = 1:size(Markers,1)
     end
 end
 
+%%
 
+% Let's look at just the waist motion, assuming that we'll be stratpping
+% the "rocket" to the person at approximately the waist.
+
+hips = [lhip rhip]; data = Markers;
+hips = backWaist;   data = d;
+
+lxypts = squeeze(data(:,hips(1),1:2));
+rxypts = squeeze(data(:,hips(2),1:2));
+
+
+figure,plot(lxypts(:,1),lxypts(:,2))
+hold on
+plot(rxypts(:,1),rxypts(:,2),'g')
 
 
