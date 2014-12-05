5d760bd40ec355049fd9993bce435fd154315030
Andrew Pound <aepound@gmail.com>
2014-12-05 02:30:48 -0700

    Adding in the rocket model.

---
+++ proj/matlab/rocket.m
@@ -0,0 +1,192 @@
+function [sys,x0,str,ts,simStateCompliance] = rocket(t,x,u,flag,P)
+
+switch flag,
+
+  %%%%%%%%%%%%%%%%%%
+  % Initialization %
+  %%%%%%%%%%%%%%%%%%
+  case 0,
+    [sys,x0,str,ts,simStateCompliance]=mdlInitializeSizes(P);
+
+  %%%%%%%%%%%%%%%
+  % Derivatives %
+  %%%%%%%%%%%%%%%
+  case 1,
+    sys=mdlDerivatives(t,x,u,P);
+
+  %%%%%%%%%%
+  % Update %
+  %%%%%%%%%%
+  case 2,
+    sys=mdlUpdate(t,x,u);
+
+  %%%%%%%%%%%
+  % Outputs %
+  %%%%%%%%%%%
+  case 3,
+    sys=mdlOutputs(t,x,u,P);
+
+  %%%%%%%%%%%%%%%%%%%%%%%
+  % GetTimeOfNextVarHit %
+  %%%%%%%%%%%%%%%%%%%%%%%
+  case 4,
+    sys=mdlGetTimeOfNextVarHit(t,x,u);
+
+  %%%%%%%%%%%%%
+  % Terminate %
+  %%%%%%%%%%%%%
+  case 9,
+    sys=mdlTerminate(t,x,u);
+
+  %%%%%%%%%%%%%%%%%%%%
+  % Unexpected flags %
+  %%%%%%%%%%%%%%%%%%%%
+  otherwise
+    DAStudio.error('Simulink:blocks:unhandledFlag', num2str(flag));
+
+end
+
+% end sfuntmpl
+
+%
+%=============================================================================
+% mdlInitializeSizes
+% Return the sizes, initial conditions, and sample times for the S-function.
+%=============================================================================
+%
+function [sys,x0,str,ts,simStateCompliance]=mdlInitializeSizes(P)
+
+%
+% call simsizes for a sizes structure, fill it in and convert it to a
+% sizes array.
+%
+% Note that in this example, the values are hard coded.  This is not a
+% recommended practice as the characteristics of the block are typically
+% defined by the S-function parameters.
+%
+sizes = simsizes;
+
+sizes.NumContStates  = 4;
+sizes.NumDiscStates  = 0;
+sizes.NumOutputs     = 2;
+sizes.NumInputs      = 2;
+sizes.DirFeedthrough = 1;
+sizes.NumSampleTimes = 1;   % at least one sample time is needed
+
+sys = simsizes(sizes);
+
+%
+% initialize the initial conditions
+%
+x0  = P.x0;
+
+%
+% str is always an empty matrix
+%
+str = [];
+
+%
+% initialize the array of sample times
+%
+ts  = [0 0];
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+simStateCompliance = 'UnknownSimState';
+
+% end mdlInitializeSizes
+
+%
+%=============================================================================
+% mdlDerivatives
+% Return the derivatives for the continuous states.
+%=============================================================================
+%
+function sys=mdlDerivatives(t,x,u,P)
+persistent t0;
+
+if ~exist('t0','var')
+    t0 = 0;
+end
+xdot=zeros(4,1);
+theta=x(1);
+thetadot=x(2);
+phi = x(3);
+phidot = x(4);
+lamda=P.lambda;
+
+% These are the desired outputs...
+theta_c = u(1);
+phi_c = u(2);
+
+% So, let's move the state one step towards the desired output...
+theta_dir = theta_c - theta;
+phi_dir   = phi_c - phi;
+
+new_theta = theta + theta_dir*lambda;
+new_phi   = phi + phi_dir*lambda;
+
+xdot(1) = new_theta;
+xdot(2) = (new_theta - theta)./(t - t0);
+xdot(3) = new_phi;
+xdot(4) = (new_phi - phi)./(t - t0);
+
+sys = xdot;
+
+% end mdlDerivatives
+
+%
+%=============================================================================
+% mdlUpdate
+% Handle discrete state updates, sample time hits, and major time step
+% requirements.
+%=============================================================================
+%
+function sys=mdlUpdate(t,x,u)
+
+sys = [];
+
+% end mdlUpdate
+
+%
+%=============================================================================
+% mdlOutputs
+% Return the block outputs.
+%=============================================================================
+%
+function sys=mdlOutputs(t,x,u,P)
+
+sys = x([1; 3]);
+
+% end mdlOutputs
+
+%
+%=============================================================================
+% mdlGetTimeOfNextVarHit
+% Return the time of the next hit for this block.  Note that the result is
+% absolute time.  Note that this function is only used when you specify a
+% variable discrete-time sample time [-2 0] in the sample time array in
+% mdlInitializeSizes.
+%=============================================================================
+%
+function sys=mdlGetTimeOfNextVarHit(t,x,u)
+
+sampleTime = 1;    %  Example, set the next hit to be one second later.
+sys = t + sampleTime;
+
+% end mdlGetTimeOfNextVarHit
+
+%
+%=============================================================================
+% mdlTerminate
+% Perform any end of simulation tasks.
+%=============================================================================
+%
+function sys=mdlTerminate(t,x,u)
+
+sys = [];
+
+% end mdlTerminate
